43e0c3b73af0623b8ab9f2f0d9e1288ad389f7cb
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-08-26 15:21:00 +0100

    simple web page and user form

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Form</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<form>
+		<div>
+		Name  <br>
+		<input type="text" name="name"><br><br>
+		E-mail  <br>
+		<input type="text" name="email"><br><br>
+		Choose Password  <br>
+		<input type="password" name="password"><br><br>
+		<input type="submit" value="submit">
+		</div>
+	</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>MY First HTML5</title>
+</head>
+
+<body>
+	<h1>Hello World,</h1>
+	<p>My name is <strong>Qozeem Odeniran,</strong><br>
+	and this is my first html5 program.</p>
+</body>
+
+</html>
